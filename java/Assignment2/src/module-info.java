cdb8e412116a639ca93ecf6d63bd9a9a37c6e8d7
Johnny Aracena <johnny.aracena@gmail.com>
2022-07-20 11:58:07 -0400

    deleting java folder

--- java/Assignment2/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Assignment2 {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires java.desktop;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
